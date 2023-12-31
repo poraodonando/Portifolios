9854a1e5a25a37d49ac013c575d7d05d3ed4e177
Nando Batista <webdev.physics@gmail.com>
2023-12-31 17:27:31 -0300

    up

---
+++ detona_Ralph/src/js/engine.js
@@ -0,0 +1,77 @@
+const state = {
+    view:{
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score")
+
+    },
+
+    values: {
+        
+        gameVelocity: 1500,
+        hitPosition: 0,
+        result: 0,
+        currentTme: 60,
+    },
+    actions: {
+        timerId: setInterval(randomSquare, 1000),
+        countDownTimerId: setInterval(countDown, 1000),
+      },
+}
+
+function countDown(){
+    state.values.currentTme--;
+    state.view.timeLeft.textContent = state.values.currentTme;
+
+    if(state.values.currentTme <= 0 ){
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.actions.timerId);
+        alert("Game Over" + state.values.result);
+    }
+}
+
+function randomSquare(){
+
+    state.view.squares.forEach((square) =>{
+        square.classList.remove("enemy");
+    });
+
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+
+    randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
+}
+
+
+function addListenerHitbox(){
+    //Alguem que fica "ouvindo" alguma ação
+
+    state.view.squares.forEach((square) =>{
+        square.addEventListener("mousedown", ()=>{ 
+
+            if(square.id === state.values.hitPosition){
+                state.values.result++;
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+            }
+
+        })
+    });
+}
+
+function init(){
+    
+
+    addListenerHitbox()
+
+}
+
+init();
+
+/**
+ * state.view()
+ * state.value()
+ */
+
